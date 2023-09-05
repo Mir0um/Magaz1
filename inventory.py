7b615c8c5c5e861e53d39c4a57dc3240ac7245c9
SARTORIS Jean-Paul.s.i <116564785+Mir0um@users.noreply.github.com>
2023-09-05 16:46:00 +0200

    fix 1

--- inventory.py
+++ inventory.py
@@ -1,9 +1,20 @@
-#(contenant les fonctions liées à la gestion de l'inventaire) :
+import json
 from typing import Dict
 
 class InventoryManager:
     def __init__(self):
-        self.inventory: Dict[str, int] = {"apple": 50, "banana": 25, "orange": 33}
+        self.inventory: Dict[str, int] = self.load_inventory_from_file("inventory.json")
+
+    def save_inventory_to_file(self, filename: str) -> None:
+        with open(filename, 'w') as file:
+            json.dump(self.inventory, file)
+
+    def load_inventory_from_file(self, filename: str) -> Dict[str, int]:
+        try:
+            with open(filename, 'r') as file:
+                return json.load(file)
+        except FileNotFoundError:
+            return {"apple": 0, "banana": 0, "orange": 0}
 
     def is_item_available(self, item_id: str, quantity: int) -> bool:
         return item_id in self.inventory.keys() and self.inventory[item_id] >= quantity
@@ -11,6 +22,7 @@ class InventoryManager:
     def purchase_item(self, item_id: str, quantity: int) -> bool:
         if item_id in self.inventory and self.inventory[item_id] >= quantity:
             self.inventory[item_id] -= quantity
+            self.save_inventory_to_file("inventory.json")
             return True
         return False
 
@@ -18,3 +30,4 @@ class InventoryManager:
         for item_id, quantity in purchase_list:
             if item_id in self.inventory.keys() and self.inventory[item_id] >= quantity:
                 self.inventory[item_id] -= quantity
+                self.save_inventory_to_file("inventory.json")
