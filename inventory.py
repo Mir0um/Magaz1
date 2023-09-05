0bbb65bf30229dc62b8847d625e99d5222af31c8
SARTORIS Jean-Paul.s.i <116564785+Mir0um@users.noreply.github.com>
2023-09-05 16:53:16 +0200

    fix2

--- inventory.py
+++ inventory.py
@@ -1,20 +1,17 @@
 import json
 from typing import Dict
+from inventory_persistence import save_inventory_to_file, load_inventory_from_file
 
 class InventoryManager:
     def __init__(self):
         self.inventory: Dict[str, int] = self.load_inventory_from_file("inventory.json")
 
-    def save_inventory_to_file(self, filename: str) -> None:
-        with open(filename, 'w') as file:
-            json.dump(self.inventory, file)
+    def save_inventory(self):
+        save_inventory_to_file(self.inventory, "inventory.json")
 
-    def load_inventory_from_file(self, filename: str) -> Dict[str, int]:
-        try:
-            with open(filename, 'r') as file:
-                return json.load(file)
-        except FileNotFoundError:
-            return {"apple": 0, "banana": 0, "orange": 0}
+    def load_inventory_from_file(self, filename):
+        return load_inventory_from_file(filename)
+    
 
     def is_item_available(self, item_id: str, quantity: int) -> bool:
         return item_id in self.inventory.keys() and self.inventory[item_id] >= quantity
